6a132ed48fa6651a7e8ea15f3a5e85f5955588cb
steftinsley <69828366+steftinsley@users.noreply.github.com>
2021-03-21 11:01:51 -0500

    fixed error in file name

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,17 +5,17 @@ function ContactForm() {
     <section>
       <h1>Contact Me</h1>
       <form id="contact-form">
-        //name input
+        
         <div>
           <label htmlFor="name">Name:</label>
           <input type="text" name="name"/>
         </div>
-        //email input
+        
         <div>
           <label htmlFor="email">Email Address:</label>
           <input type="email" name="email"/>
         </div>
-        //message text area
+        
         <div>
           <label htmlFor="message">Message:</label>
           <textarea name="message" rows="5"  />
