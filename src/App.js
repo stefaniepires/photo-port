0a0afd9af3a06316da33f165b01ac2f15a905ca8
steftinsley <69828366+steftinsley@users.noreply.github.com>
2021-03-22 10:14:30 -0500

    added modal

--- src/App.js
+++ src/App.js
@@ -16,29 +16,31 @@ function App() {
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
       <Nav
-  categories={categories}
-  setCurrentCategory={setCurrentCategory}
-  currentCategory={currentCategory}
-  contactSelected={contactSelected}
-  setContactSelected={setContactSelected}
-></Nav>
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
       <main>
-      {!contactSelected ? (
-  <>
-    <Gallery currentCategory={currentCategory}></Gallery>
-    <About></About>
-  </>
-) : (
-    <ContactForm></ContactForm>
-  )}
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
 }
 
 export default App;
+
